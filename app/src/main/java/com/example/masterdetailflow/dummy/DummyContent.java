e1f1735a3d15a5ec53665bfcadc47cc557c4f6ac
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-04 14:32:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/masterdetailflow/dummy/DummyContent.java
@@ -0,0 +1,75 @@
+package com.example.masterdetailflow.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+    private static final int COUNT = 25;
+
+    static {
+        addItem(new DummyItem("1", "Bucky", "http://samethinking.com/videos.php?cat=278&video=27365"));
+        addItem(new DummyItem("2", "Forum", "http://samethinking.com/forum/"));
+        addItem(new DummyItem("1", "Video", "http://samethinking.com/videos.php"));
+        // Add some sample items.
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createDummyItem(i));
+        }
+    }
+
+    private static void addItem(DummyItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    private static DummyItem createDummyItem(int position) {
+        return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class DummyItem {
+        public final String id;
+        public final String item_name;
+        public final String url;
+
+        public DummyItem(String id, String item_name, String url) {
+            this.id = id;
+            this.item_name = item_name;
+            this.url = url;
+        }
+
+        @Override
+        public String toString() {
+            return item_name;
+        }
+    }
+}
